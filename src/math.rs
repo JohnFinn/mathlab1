eb82d8c1cddd14d90d95af16905511b5c687ea38
jouny <dz4tune@gmail.com>
2019-03-03 15:11:41 +0300

    intervals function

--- src/math.rs
+++ src/math.rs
@@ -3,12 +3,23 @@ extern crate statistical;
 use std::ops::Deref;
 use statistical::*;
 
+pub fn intervals<T: Deref<Target=[f32]>>(collection: &T, interval: f32) -> Vec<i32> {
+    let min = min(collection);
+    let parts = ((max(collection) - min) / interval).ceil() as usize;
+    let mut result = Vec::new();
+    result.resize(parts, 0);
+    for &elem in collection.iter() {
+        let position = ((elem - min) / interval) as usize;
+        result[position] += 1;
+    }
+    result
+}
 
 pub fn min<T: Deref<Target=[f32]>>(collection: &T) -> f32 {
     let mut current = std::f32::MAX;
-    for x in collection.iter() {
-        if *x < current {
-            current = *x;
+    for &x in collection.iter() {
+        if x < current {
+            current = x;
         }
     }
     current
@@ -16,9 +27,9 @@ pub fn min<T: Deref<Target=[f32]>>(collection: &T) -> f32 {
 
 pub fn max<T: Deref<Target=[f32]>>(collection: &T) -> f32 {
     let mut current = std::f32::MIN;
-    for x in collection.iter() {
-        if *x > current {
-            current = *x;
+    for &x in collection.iter() {
+        if x > current {
+            current = x;
         }
     }
     current
@@ -31,4 +42,17 @@ pub fn standart_error<T: Deref<Target=[f32]>>(collection: &T) -> f32 {
 pub fn probability_less_than<T: Deref<Target=[f32]>>(x: f32, collection: &T) -> f32 {
     let less_than_x_count = collection.iter().filter(|a| **a < x).count();
     less_than_x_count as f32 / collection.len() as f32
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn intervals_test() {
+        let a = vec![1.0, 1.1, 2.1, 3.2, 3.9];
+        assert_eq!(intervals(&a, 1.0), vec![2, 1, 2]);
+        let a = vec![1.0, 1.1, 3.0, 3.2];
+        assert_eq!(intervals(&a, 1.0), vec![2, 0, 2]);
+    }
 }
